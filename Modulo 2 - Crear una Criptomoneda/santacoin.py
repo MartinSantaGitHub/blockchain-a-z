211a1b41dd99d0a1da8924a54a16df665c2f0c0c
martins <martin.santamaria@rga.com>
2021-12-03 14:11:04 -0300

    Add new items

---
+++ Modulo 2 - Crear una Criptomoneda/santacoin.py
@@ -0,0 +1,116 @@
+# region Libraries
+
+import datetime
+import hashlib
+import json
+import requests
+from urllib.parse import urlparse
+
+# endregion
+
+# region Create a cryptocoin
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+        self.create_block(proof=1, previous_hash='0')
+        self.nodes = set()
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+
+        while not check_proof:
+            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+
+            if hash_operation[:4] == '0' * 4:
+                check_proof = True
+            else:
+                new_proof += 1
+
+        return new_proof
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+
+        while block_index < len(chain):
+            current_block = chain[block_index]
+
+            if current_block['previous_hash'] != self.hash(previous_block):
+                return False
+
+            previous_proof = previous_block['proof']
+            proof = current_block['proof']
+            hash_operation = hashlib.sha256(str(proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+
+            if hash_operation[:4] != '0' * 4:
+                return False
+
+            previous_block = current_block
+            block_index += 1
+
+        return True
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'previous_hash': previous_hash,
+            'transactions': self.transactions
+        }
+        self.transactions = []
+        self.chain.append(block)
+
+        return block
+
+    def get_last_block(self):
+        return self.chain[-1]
+
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append({'sender': sender,
+                                  'receiver': receiver,
+                                  'amount': amount})
+
+        last_block = self.get_last_block()
+
+        return last_block['index'] + 1
+
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+
+        self.nodes.add(parsed_url)
+
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+
+        for node in network:
+            response = requests.get(f'{node.scheme}://{node.netloc}/get_chain')
+
+            if response.status_code == 200:
+
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+
+        if longest_chain:
+            self.chain = longest_chain
+
+            return True
+
+        return False
+
+# endregion
---
+++ Modulo 2 - Crear una Criptomoneda/santacoin_node_5001.py
@@ -0,0 +1,116 @@
+import datetime
+from flask import Flask, jsonify, request
+from santacoin import Blockchain
+from uuid import uuid4
+
+# Create a Web App
+app = Flask("foo")
+
+# Create the node's address in the port 5000
+node_address = str(uuid4()).replace('-', '')
+
+# Create a Blockchain
+blockchain = Blockchain()
+
+
+# Mine a new block
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    last_block = blockchain.get_last_block()
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+    last_hash = blockchain.hash(last_block)
+
+    blockchain.add_transaction(sender=node_address, receiver='Santa', amount=10)
+
+    block = blockchain.create_block(proof, last_hash)
+
+    response = {
+        'message': 'Congrats, you just mined a new block!',
+        'block': block
+    }
+
+    return jsonify(response), 201
+
+
+# Get the complete blockchain
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    chain = blockchain.chain
+
+    response = {
+        'length': len(chain),
+        'chain': chain
+    }
+
+    return jsonify(response), 200
+
+
+# Validate the blockchain
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    response = {
+        'is_valid': blockchain.is_chain_valid(),
+        'timestamp': datetime.datetime.now()
+    }
+
+    return jsonify(response), 200
+
+
+# Add a new transaction to the blockchain
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+
+    if not all(key in json for key in transaction_keys):
+        return 'Missing elements for the transaction', 400
+
+    index = blockchain.add_transaction(sender=json['sender'], receiver=json['receiver'], amount=json['amount'])
+    response = {
+        'message': f'The transaction will be added to the block number #{index}'
+    }
+
+    return jsonify(response), 201
+
+
+# region Decentralize the blocks' chains
+
+# Connect new nodes
+@app.route('/connect_node', methods=['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+
+    if nodes is None:
+        return 'There is no nodes to add', 400
+
+    for node in nodes:
+        blockchain.add_node(node)
+
+    response = {
+        'message': 'All the nodes were connected. The Santa blockchain contains the next nodes in \'total_nodes\'',
+        'total_nodes': list(nodes)}
+
+    return jsonify(response), 201
+
+
+# Replace the chain for the longest one (if necessary)
+@app.route('/replace_chain', methods=['GET'])
+def replace_chain():
+    is_replace = blockchain.replace_chain()
+
+    if is_replace:
+        response = {'message': 'The nodes had different chains, and they were all replaced for the longest one.',
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': 'Everything OK. The chain in all the nodes is already the longest one.',
+                    'current_chain': blockchain.chain}
+
+    return jsonify(response), 200
+
+
+# endregion
+
+# Execute the web app
+app.run(host='0.0.0.0', port=5001)
